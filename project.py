a1384be079434a4ec4576d4bc3a5331b3c6a4578
Ahmed Cherif <30884697+RoyalFlush24@users.noreply.github.com>
2017-12-27 21:39:35 -0600

    Changed line 51

--- project.py
+++ project.py
@@ -42,12 +42,10 @@ session = DBSession()
 
 # This is where we create anti-forgery state token
 
-
 @app.route('/login')
 def showLogin():
-    state = ''.join(
-        random.choice(string.ascii_uppercase + string.digits)
-        for x in range(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
